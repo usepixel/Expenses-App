84332cacd578a8a70e9d59c3bb9e34cc8c168cbc
Владимир <126570358+usepixel@users.noreply.github.com>
2025-02-11 21:27:19 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
-const inputNode = document.querySelector('expenses__input');
-const limitNode = document.querySelector('expenses__limit');
-const totalNode = document.querySelector('expenses__total');
-const statusNode = document.querySelector('expenses__status');
-const buttonNode = document.querySelector('expenses__button');
-const historyNode = document.querySelector('expenses__history');
+const inputNode = document.querySelector('.expenses__input');
+const limitNode = document.querySelector('.expenses__limit');
+const totalNode = document.querySelector('.js-total');
+const statusNode = document.querySelector('.expenses__status');
+const buttonNode = document.querySelector('.expenses__button');
+const historyNode = document.querySelector('.expenses__history-list');
 
-const valueUser = [];
+
+const userValue = [];
 
 buttonNode.addEventListener('click', () => {
-    getValueFromUser(inputNode);
-});
+    const money = convertToNumber(inputNode);
+    if(!money){
+        console.log('error');
+        return;
+    }
+    saveNumberToArr(money);
+    let total = calcNumberArr();
+    renderTotal(total);
+    inputNode.value = '';
+})
 
-function getValueFromUser(inputNode){
-    valueUser.push(inputNode.value);
-    console.log(valueUser)
+function convertToNumber(inputNode){
+    return parseInt(inputNode.value);
 }
+
+function saveNumberToArr(money){
+    userValue.push(money);
+}
+
+function calcNumberArr(){
+    let result = 0;
+    userValue.forEach(function(any){
+        result += any;
+    })
+    return result;
+}
+
+function renderTotal(total){ // Выводит полную сумму всех введеных значений.
+    totalNode.textContent = total + 'руб.';
+};
